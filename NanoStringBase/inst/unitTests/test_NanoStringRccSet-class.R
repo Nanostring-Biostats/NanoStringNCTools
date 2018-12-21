3873021ec83f68d7bb769128e4c85fb02f88366d
Patrick Aboyoun <paboyoun@nanostring.com>
2018-12-21 15:50:55 -0800

    Add a second unit test to NanoStringRccSet constructor

--- NanoStringBase/inst/unitTests/test_NanoStringRccSet-class.R
+++ NanoStringBase/inst/unitTests/test_NanoStringRccSet-class.R
@@ -1,3 +1,45 @@
-test_NanoStringRccSet_construction <- function() {
+test_NanoStringRccSet_constructor_empty <- function() {
   checkTrue(validObject(NanoStringRccSet()))
+}
+
+rcc <-
+  list(assayData =
+         matrix(0:11, 4L, 3L,
+                dimnames = list(letters[1:4], sprintf("%s.RCC", LETTERS[1:3]))),
+       featureData =
+         AnnotatedDataFrame(data.frame(BarcodeClass = c("Endogenous", "Positive", "Negative", "Housekeeping"),
+                                       GeneName = letters[1:4],
+                                       Accession = letters[1:4],
+                                       row.names = letters[1:4],
+                                       stringsAsFactors = FALSE),
+                            dimLabels = c("featureNames", "featureColumns")),
+       annotation = "rlffile",
+       protocolData =
+         AnnotatedDataFrame(data.frame(FileVersion = numeric_version(rep("1.7", 3L)),
+                                       SoftwareVersion = numeric_version(rep("4.0.0.3", 3L)),
+                                       SystemType = rep("Gen2", 3L),
+                                       SampleID = letters[1:3],
+                                       Owner = rep("", 3L),
+                                       Comments = rep("DNA-RNA-Protein", 3L),
+                                       Date = as.Date(rep("1999-12-31", 3L)),
+                                       SystemAPF = rep("n6_vDV1", 3L),
+                                       AssayType = rep(NA_character_, 3L),
+                                       LaneID = 1:3,
+                                       FovCount = rep(280L, 3L),
+                                       FovCounted = 1:3,
+                                       ScannerID = rep("a", 3L),
+                                       StagePosition = 1:3,
+                                       BindingDensity = c(0.75, 1, 1.25),
+                                       CartridgeID = letters[1:3],
+                                       CartridgeBarcode = rep("", 3L),
+                                       row.names = sprintf("%s.RCC", LETTERS[1:3]),
+                                       stringsAsFactors = FALSE),
+                            NanoStringBase:::.rccMetadata[["protocolData"]],
+                            dimLabels = c("sampleNames", "sampleColumns")))
+
+test_NanoStringRccSet_constructor_simple <- function() {
+  checkTrue(validObject(NanoStringRccSet(rcc$assayData,
+                                         featureData = rcc$featureData,
+                                         annotation = rcc$annotation,
+                                         protocolData = rcc$protocolData)))
 }
