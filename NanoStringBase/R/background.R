5ee7256ceda0cd2f08ce862086d741db11f28fdb
Patrick Aboyoun <paboyoun@nanostring.com>
2018-12-29 23:06:33 -0800

    Add fgScale function as a normalization helper funciton

--- NanoStringBase/R/background.R
+++ NanoStringBase/R/background.R
@@ -1,2 +1,4 @@
 bgThresh <- function(x, STATS) sweep(x, 2L, STATS, FUN = "pmax")
 bgSubtr  <- function(x, STATS) sweep(x, 2L, STATS, FUN = "-")
+
+fgScale <- function(x, STATS) sweep(x, 2L, STATS / mean(STATS), FUN = "/")
