5415a46d034e83dd0821891143f403593d791c14
Patrick Aboyoun <paboyoun@nanostring.com>
2018-12-31 12:05:54 -0800

    Add logt function for thresholded natural logarithm

--- NanoStringBase/R/Math.R
+++ NanoStringBase/R/Math.R
@@ -1,3 +1,12 @@
+logt <-
+function(x, thresh = 0.5)
+{
+  if (min(x, na.rm = TRUE) < thresh) {
+    x[!is.na(x) & x >= 0 & x < thresh] <- thresh
+  }
+  log(x)
+}
+
 log2t <-
 function(x, thresh = 0.5)
 {
