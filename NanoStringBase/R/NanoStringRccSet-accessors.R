317077a1a60b9e86ad1908282aded54137b2b977
Patrick Aboyoun <paboyoun@nanostring.com>
2019-01-09 13:23:52 -0800

    Move accessor methods into its own source file

---
+++ NanoStringBase/R/NanoStringRccSet-accessors.R
@@ -0,0 +1,57 @@
+setGeneric("sData", signature = "object",
+           function(object) standardGeneric("sData"))
+setMethod("sData", "NanoStringRccSet",
+          function(object) cbind(pData(object), pData(protocolData(object))))
+
+setGeneric("svarLabels", signature = "object",
+           function(object) standardGeneric("svarLabels"))
+setMethod("svarLabels", "NanoStringRccSet",
+          function(object) c(varLabels(object), varLabels(protocolData(object))))
+
+assayDataElement2 <- function(object, elt)
+{
+  if (elt %in% assayDataElementNames(object))
+    assayDataElement(object, elt)
+  else
+    stop("'elt' not present in assayData(object)")
+}
+
+setGeneric("signatureWeights", signature = "object",
+           function(object) standardGeneric("signatureWeights"))
+setMethod("signatureWeights", "NanoStringRccSet",
+          function(object) object@signatureWeights)
+
+setGeneric("signatureWeights<-", signature = c("object", "value"),
+           function(object, value) standardGeneric("signatureWeights<-"))
+setReplaceMethod("signatureWeights", c("NanoStringRccSet", "NumericList"),
+                 function(object, value) {
+                   object@signatureWeights <- value
+                   object
+                 })
+setReplaceMethod("signatureWeights", c("NanoStringRccSet", "ANY"),
+                 function(object, value) {
+                   object@signatureWeights <- as(value, "NumericList")
+                   object
+                 })
+setReplaceMethod("signatureWeights", c("NanoStringRccSet", "NULL"),
+                 function(object, value) {
+                   object@signatureWeights <- NumericList()
+                   object
+                 })
+
+setMethod("design", "NanoStringRccSet", function(object) object@design)
+setReplaceMethod("design", c("NanoStringRccSet", "formula"),
+                 function(object, value) {
+                   object@design <- value
+                   object
+                 })
+setReplaceMethod("design", c("NanoStringRccSet", "ANY"),
+                 function(object, value) {
+                   object@design <- as.formula(value)
+                   object
+                 })
+setReplaceMethod("design", c("NanoStringRccSet", "NULL"),
+                 function(object, value) {
+                   object@design <- NULL
+                   object
+                 })
