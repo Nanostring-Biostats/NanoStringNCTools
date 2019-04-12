c8c83a7c3fd53354460bed140befea500aaa42da
dnadave <dnadavenator@gmail.com>
2019-04-12 19:24:28 +0000

    Adding in functionality for groups within signatures

--- R/SignatureSet-class.R
+++ R/SignatureSet-class.R
@@ -1,11 +1,13 @@
 # Class definition
 setClass("SignatureSet",
          contains = "VersionedBiobase",
-         slots = c(weights = "NumericList"),
+         slots = c(weights = "NumericList", groups = "factor", version = "character"),
          prototype = prototype(
            new("VersionedBiobase",
                versions = c(SignatureSet = "1.0.0")),
-           weights = NumericList()))
+           weights = NumericList(),
+           groups = factor(),
+           version = character()))
 
 # Initialization method
 setMethod("initialize", "SignatureSet",
@@ -13,8 +15,32 @@ function(.Object, weights = NumericList(), ...)
 {
   callNextMethod(.Object,
                  weights = as(weights, "NumericList"),
+                 groups = as( names( weights ) , "factor" ),
+                 version = "0.0.1",
                  ...)
 })
+
+# Initialization method
+setMethod("initialize", "SignatureSet",
+          function(.Object, weights = NumericList(), groups = factor(), ...)
+          {
+            callNextMethod(.Object,
+                           weights = as(weights, "NumericList"),
+                           groups = as( groups , "factor" ),
+                           version = "0.0.1",
+                           ...)
+          })
+
+# Initialization method
+setMethod("initialize", "SignatureSet",
+          function(.Object, weights = NumericList(), groups = factor(), version = character(), ...)
+          {
+            callNextMethod(.Object,
+                           weights = as(weights, "NumericList"),
+                           groups = ifelse( inherits( groups , "factor" ) , as( groups , "factor" ) , factor( groups ) ),
+                           version = as( version , "character" ),
+                           ...)
+          })
 
 # Show method
 setMethod("show", signature = "SignatureSet",
@@ -33,6 +59,18 @@ function(weights = NumericList(), ...)
 {
   new2("SignatureSet", weights = weights, ...)
 }
+
+SignatureSet <-
+  function(weights = NumericList(), groups = factor(), ...)
+  {
+    new2("SignatureSet", weights = weights , groups = groups, ...)
+  }
+
+SignatureSet <-
+  function(weights = NumericList(), groups = factor(), version = character(), ...)
+  {
+    new2("SignatureSet", weights = weights , groups = groups, version = version, ...)
+  }
 
 # Weights Accessor and Replacer
 setMethod("weights", "SignatureSet",
@@ -68,7 +106,55 @@ setReplaceMethod("weights", c("SignatureSet", "NULL"),
                    object
                  })
 
+# Groups Accessor and Replacer
+setMethod("groups", "SignatureSet",
+          function(object, ...) object@groups)
+
+setGeneric("groups<-", signature = c("object", "value"),
+           function(object, value) standardGeneric("groups<-"))
+setReplaceMethod("groups", c("SignatureSet", "factor"),
+                 function(object, value) {
+                   object@groups <- value
+                   object
+                 })
+setReplaceMethod("groups", c("SignatureSet", "ANY"),
+                 function(object, value) {
+                   object@groups <- ifelse( inherits( value , "factor" ) , as( value , "factor" ) , factor( value ) )
+                   object
+                 })
+setReplaceMethod("groups", c("SignatureSet", "NULL"),
+                 function(object, value) {
+                   object@groups <- factor()
+                   object
+                 })
+
+# Version Accessor and Replacer
+setMethod("version", "SignatureSet",
+          function(object, ...) object@version)
+
+setGeneric("version<-", signature = c("object", "value"),
+           function(object, value) standardGeneric("version<-"))
+setReplaceMethod("version", c("SignatureSet", "character"),
+                 function(object, value) {
+                   object@version <- value
+                   object
+                 })
+setReplaceMethod("version", c("SignatureSet", "ANY"),
+                 function(object, value) {
+                   object@version <- as(value, "character")
+                   object
+                 })
+setReplaceMethod("version", c("SignatureSet", "NULL"),
+                 function(object, value) {
+                   object@version <- character()
+                   object
+                 })
+
 # Additional methods
+setMethod("version", "SignatureSet",
+          function( object ) {
+            return( object@version )
+          })
 setMethod("length", "SignatureSet",
           function(x) {
             length(weights(x))
@@ -83,3 +169,25 @@ setMethod("lengths", "SignatureSet",
                    function(x) sum(names(x) != "(Intercept)"),
                    USE.NAMES = use.names)
           })
+
+## Set validity functions to check for continuity between weigthts and groups
+## A valid "track" object has the same number of x, y values
+validSignatureSet <- function( object )
+{
+  errorMessage <- NULL
+  if ( length( object@weights ) != length( object@groups ) )
+  {
+    errorMessage <- paste( "Unequal weights and groups.  Lengths: " , length( object@weights ) , ", " , length( object@groups ) , sep="" )
+  }
+  if ( length( object@weights ) != length( union( names( object@weights ) , names( object@groups ) ) ) )
+  {
+    errorMessage <- switch( is.null( errorMessage ),
+                            paste( "Unequal weights and groups.  Lengths: " , length( object@weights ) , ", " , length( object@groups ) , sep="" ) ,
+                            paste( errorMessage , "Weight names do not match group member names" , sep = "\n" )
+    )
+  }
+  ifelse( is.null( errorMessage ) , TRUE , errorMessage )
+}
+
+## assign the function as the validity method for the class
+setValidity( "SignatureSet" , validSignatureSet )
